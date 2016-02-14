e30fea65d305d75fa5655add31f2c06423fe87a2
Takk-Tikk <takuma.substance@gmail.com>
2016-02-14 19:58:30 +0900

    Initial Commit

---
+++ Ulam'sSpiral/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ulam'sSpiral
+//
+//  Created by 小倉拓馬 on 2016/02/14.
+//  Copyright © 2016年 Takuma Ogura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Ulam'sSpiralTests/Ulam_sSpiralTests.swift
@@ -0,0 +1,36 @@
+//
+//  Ulam_sSpiralTests.swift
+//  Ulam'sSpiralTests
+//
+//  Created by 小倉拓馬 on 2016/02/14.
+//  Copyright © 2016年 Takuma Ogura. All rights reserved.
+//
+
+import XCTest
+@testable import Ulam_sSpiral
+
+class Ulam_sSpiralTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
